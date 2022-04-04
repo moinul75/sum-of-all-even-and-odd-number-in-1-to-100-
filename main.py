d064e875c81d1b654ee29706ed706931f0880b64
moinul75 <102654562+moinul75@users.noreply.github.com>
2022-04-04 14:10:52 +0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+# sum of all odd number in 1 to 100
+
+sum = 0
+i = 1
+
+while i<= 100:
+    sum = sum + i
+    i= i+2
+print(sum)
+
+# sum of all even number in 1 to 100
+
+sum = 0
+i = 2
+while i<= 100:
+    sum = sum + i
+    i+=2
+print(sum)
